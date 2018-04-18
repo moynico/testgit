a5f3ef96b36caa20b47dd96dc6acfd3353503a78
Moises <mnancula@alumnos.ubiobio.cl>
2018-04-18 14:38:54 -0300

    De 25 para arriba

---
+++ testgit.c
@@ -0,0 +1,19 @@
+
+class CalculoIMC{
+	int peso;
+	int altura;
+	double imc;
+	
+	imc = peso/;
+	
+	if(imc>30){
+		printf("Es obeso/a");
+		}
+	if (imc>25&&imc<30){
+		printf("Tiene sobrepeso");
+	}
+	if(){
+		printf();
+	}
+	
+};
